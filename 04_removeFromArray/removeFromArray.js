49a9a4309acc88a6d9c09dca40bb75700e4de163
YonYoon <zhzhzh1806@gmail.com>
2023-07-10 11:07:14 +0600

    changed solution of 4th task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,13 @@
-const removeFromArray = function (array, ...numbers) {
-    for (var j = 0; j < numbers.length; j++) {
-        for (var i = 0; i < array.length; i++) {
-            if (array[i] === numbers[j]) {
-                array.splice(i, 1)
-                break
-            }
-            else { continue }
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
         }
-    }
-    return array
+    })
+    
+    return newArray;
 };
 
 // Do not edit below this line
